1a6b9293c438f84179a48c6782f471b4f097d4e6
arujasaraf <65903153+arujasaraf@users.noreply.github.com>
2020-07-25 14:41:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var gameState=0;
+var playerCount;
+var database,game,form,player;
+var allPlayers;
+var distance=0
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+  
+    game=new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+    if (playerCount===4){
+        game.update(1);
+    }
+    if (gameState===1){
+        clear();
+        game.play();
+    }
+   drawSprites();
+} 
